00db50619cc184a142ea8af8a9cf09155967007e
JangWonJong <jwj9922@naver.com>
2022-06-18 17:10:35 +0900

    mibot ERD first

---
+++ src/main/java/kr/co/eis/api/image/domains/Image.java
@@ -0,0 +1,35 @@
+package kr.co.eis.api.image.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+/**
+ * packageName: kr.co.mibot.api.image
+ * fileName   : Image
+ * author     : Jangwonjong
+ * date       : 2022-06-18
+ * desc       :
+ * ======================================
+ * DATE          AUTHOR            NOTE
+ * ======================================
+ * 2022-06-18     Jangwonjong       최초 생성
+ */
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @Column(name = "image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private long imageId;
+
+}
